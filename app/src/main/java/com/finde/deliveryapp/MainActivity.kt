41d2962e70f6a0212b94f76533f93c480d87530d
Johnson Osei Yeboah <johnyoat@gmail.com>
2020-01-31 02:20:53 +0000

    Home Preview

--- app/src/main/java/com/finde/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/finde/deliveryapp/MainActivity.kt
@@ -2,11 +2,64 @@ package com.finde.deliveryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Mapbox.getInstance(this,getString(R.string.map_box_token))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync { mapboxMap ->
+            mapboxMap.uiSettings.isAttributionEnabled = false
+
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS
+            ) { }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
